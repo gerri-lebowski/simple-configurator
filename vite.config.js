50cc6c367ad3da81f009ab7a019986deba60b16e
gerri-lebowski <86618469+gerri-lebowski@users.noreply.github.com>
2025-05-22 09:46:07 +0200

    fix: vite base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "./",
   publicDir: "public",
-  base: "/simple-configurator/",
+  base: "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
